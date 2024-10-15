9107169846f24b101ea1a7bdd4736efd968c7beb
Nathan PICOT <nathan.picot43@gmail.com>
2024-10-15 16:17:42 +0200

    ajout des prévision sur 5 jours affiché 3h par 3h

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
+  const [forecastData, setForecastData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -20,9 +21,12 @@ function App() {
       const { latitude, longitude } = position.coords;
       const API_KEY = '7ada24f6c49b3f4638c8db65468fd13f';
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`;
+      const previsionUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`;
 
       try {
         const response = await axios.get(url);
+        const forecastResponse = await axios.get(previsionUrl);
+        setForecastData(forecastResponse.data);
         setWeatherData(response.data);
         setLoading(false);
       } catch (err) {
@@ -42,14 +46,38 @@ function App() {
   return (
       <div className="App">
         <header className="App-header">
-          {loading && <p>Chargement...</p>}
-          {error && <p>{error}</p>}
+          {loading && <p className="loading">Chargement...</p>}
+          {error && <p className="error">{error}</p>}
+
           {weatherData && (
-              <div>
+              <div className="current-weather">
                 <h1>Météo actuelle</h1>
                 <p>Lieu: {weatherData.name}</p>
                 <p>Température: {weatherData.main.temp}°C</p>
                 <p>Condition: {weatherData.weather[0].description}</p>
+                <img
+                    src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                    alt={weatherData.weather[0].description}
+                />
+              </div>
+          )}
+
+          {forecastData && (
+              <div>
+                <h2>Prévisions météo</h2>
+                <div className="carousel">
+                  {forecastData.list.map((item, index) => (
+                      <div key={index} className="forecast-item">
+                        <h3>Prévision pour {item.dt_txt}</h3>
+                        <p>Température: {item.main.temp}°C</p>
+                        <p>Condition: {item.weather[0].description}</p>
+                        <img
+                            src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                            alt={item.weather[0].description}
+                        />
+                      </div>
+                  ))}
+                </div>
               </div>
           )}
         </header>
