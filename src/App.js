59ec60407bce667af2c2baef2cc9623f03c3df49
Nathan PICOT <nathan.picot43@gmail.com>
2024-10-16 11:00:13 +0200

    correction css affichage separation des jours pour les prévisions

--- src/App.js
+++ src/App.js
@@ -43,6 +43,25 @@ function App() {
     getUserLocation();
   }, []);
 
+  // Fonction pour grouper les prévisions par jour
+  const groupForecastByDay = (forecastList) => {
+    const grouped = forecastList.reduce((grouped, forecast) => {
+      const date = forecast.dt_txt.split(' ')[0]; // Obtenir seulement la partie "YYYY-MM-DD"
+      if (!grouped[date]) {
+        grouped[date] = [];
+      }
+      grouped[date].push(forecast);
+      return grouped;
+    }, {});
+
+    // Retourner un tableau d'objets {date, forecasts}, et retirer le dernier jour avec slice
+    const days = Object.entries(grouped)
+        .map(([date, forecasts]) => ({ date, forecasts }))
+        .slice(0, -1); // Enlever le dernier jour
+
+    return days;
+  };
+
   return (
       <div className="App">
         <header className="App-header">
@@ -65,19 +84,24 @@ function App() {
           {forecastData && (
               <div>
                 <h2>Prévisions météo</h2>
-                <div className="carousel">
-                  {forecastData.list.map((item, index) => (
-                      <div key={index} className="forecast-item">
-                        <h3>Prévision pour {item.dt_txt}</h3>
-                        <p>Température: {item.main.temp}°C</p>
-                        <p>Condition: {item.weather[0].description}</p>
-                        <img
-                            src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
-                            alt={item.weather[0].description}
-                        />
+                {groupForecastByDay(forecastData.list).map((day, index) => (
+                    <div key={index} className="daily-forecast">
+                      <h3>{day.date}</h3>
+                      <div className="carousel">
+                        {day.forecasts.map((item, idx) => (
+                            <div key={idx} className="forecast-item">
+                              <h3>Prévision pour {item.dt_txt}</h3>
+                              <p>Température: {item.main.temp}°C</p>
+                              <p>Condition: {item.weather[0].description}</p>
+                              <img
+                                  src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                                  alt={item.weather[0].description}
+                              />
+                            </div>
+                        ))}
                       </div>
-                  ))}
-                </div>
+                    </div>
+                ))}
               </div>
           )}
         </header>
